3455f958a63c28095d17cebdb84cda0ce92d83ca
Serenely <sgilovboris12@gmail.com>
2021-10-16 21:36:00 +0300

    add if/else/case

--- script.js
+++ script.js
@@ -8,12 +8,54 @@ const personalMovieDB = {
     privat: false
 }
 
-const first = prompt('Один из последних фильмов, которые Вы смотрели?', '');
-const second = prompt('На сколько оцените его?', '');
-const third = prompt('Один из последних фильмов, которые Вы смотрели?', '');
-const forth = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[first] = second;
-personalMovieDB.movies[third] = forth;
+
+for (let i = 0; i < 2; i++) {
+    const first = prompt('Один из последних фильмов, которые Вы смотрели?', ''),
+        second = prompt('На сколько оцените его?', '');
+
+    if (first != null && first != '' && first.length < 50 && second != null && second != '' && second.length < 50) {
+
+        personalMovieDB.movies[first] = second;
+        console.log('perfect');
+    } else {
+        console.log('problems');
+        i--;
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count < 1000) {
+    console.log('Вы киноман');
+} else {
+    console.log('Ошибочка вышла');
+};
+switch (personalMovieDB.count) {
+    case 42:
+        console.log('Юбью Киську');
+}
 
 console.log(personalMovieDB);
+
+
+
+
+
+// for (let i = 0; i < 2; i++) {
+//     const first = prompt('Один из последних фильмов, которые Вы смотрели?', ''),
+//         second = prompt('На сколько оцените его?', '');
+
+//     if (first != null && second != null && first != '' && second != '' && first.length < 50 && second.length < 50 && first != 'Киська' && second != 'Киська') {
+
+//         personalMovieDB.movies[first] = second;
+//         console.log('done');
+
+//     } else {
+//         console.log('eror');
+//         i--;
+//     }
+// }
